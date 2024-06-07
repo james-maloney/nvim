fc11879dd58c77a29c25bf5e4904e82bd620332c
James Maloney <jamessagan@gmail.com>
2024-06-07 11:09:16 -0600

    set options

--- init.lua
+++ init.lua
@@ -37,31 +37,3 @@ require "nvchad.autocmds"
 vim.schedule(function()
   require "mappings"
 end)
-
-
-vim.opt.undofile = false
-vim.wo.relativenumber = true
-vim.wo.wrap = false
-
-vim.filetype.add({ extension = { templ = "templ" } })
-
-local custom_format = function()
-    if vim.bo.filetype == "templ" then
-        local bufnr = vim.api.nvim_get_current_buf()
-        local filename = vim.api.nvim_buf_get_name(bufnr)
-        local cmd = "templ fmt " .. vim.fn.shellescape(filename)
-
-        vim.fn.jobstart(cmd, {
-            on_exit = function()
-                -- Reload the buffer only if it's still the current buffer
-                if vim.api.nvim_get_current_buf() == bufnr then
-                    vim.cmd('e!')
-                end
-            end,
-        })
-    else
-        vim.lsp.buf.format()
-    end
-end
-
-vim.api.nvim_create_autocmd({ "BufWritePre" }, { pattern = { "*.templ" }, callback = custom_format })
--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,36 @@
 require "nvchad.options"
 
--- add yours here!
+local opt = vim.o
+opt.expandtab = false
+opt.shiftwidth = 4
+opt.smartindent = false
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.autoindent = false
+opt.undofile = false
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+vim.wo.relativenumber = true
+vim.wo.wrap = false
+
+vim.filetype.add({ extension = { templ = "templ" } })
+
+local custom_format = function()
+    if vim.bo.filetype == "templ" then
+        local bufnr = vim.api.nvim_get_current_buf()
+        local filename = vim.api.nvim_buf_get_name(bufnr)
+        local cmd = "templ fmt " .. vim.fn.shellescape(filename)
+
+        vim.fn.jobstart(cmd, {
+            on_exit = function()
+                -- Reload the buffer only if it's still the current buffer
+                if vim.api.nvim_get_current_buf() == bufnr then
+                    vim.cmd('e!')
+                end
+            end,
+        })
+    else
+        vim.lsp.buf.format()
+    end
+end
+
+vim.api.nvim_create_autocmd({ "BufWritePre" }, { pattern = { "*.templ" }, callback = custom_format })
